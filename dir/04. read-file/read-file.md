acfe247e3ea957146ebef345b18048b89a4516c2
마현우 <mhw35689402@gmail.com>
2022-11-13 22:02:21 +0900

    RESET

--- dir/04. read-file/read-file.md
+++
@@ -1,8 +0,0 @@
-# Node.js에서 파일 읽기
-
-## CRUD
-- 정보시스템에서 가지는 기본적인 데이터 처리 기능
-- C : Create
-- R : Read
-- U : Update
-- D : Delete
--- dir/04. read-file/tutorial.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-fs.readFile('sample.txt', (err, data) => {
-    if(err) throw err;
-    console.log(data);
-});
