1985b37cbc441901519d521590af7b9c533109f3
마현우 <mhw35689402@gmail.com>
2022-11-15 20:18:57 +0900

    ADD :: 404 not found 구현

--- workspace/main.js
+++ workspace/main.js
@@ -4,38 +4,38 @@ var url = require('url')
 
 var app = http.createServer((request,response) => {
     var _url = request.url;
-    var queryData = url.parse(_url, true).query
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
     var title = queryData.id;
-    console.log(title);
-    if(_url == '/'){
-      title = 'Welcome';
-    }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
-    fs.readFile(`./data/${queryData.id}`, 'utf-8', (err, description) => {
+    
+    if(pathname === '/') {
+      fs.readFile(`./data/${queryData.id}`, 'utf-8', (err, description) => {
       var templete = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ol>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ol>
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>    
-    `;
-    response.end(templete);
-    })
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ol>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ol>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>    
+      `;
+      response.writeHead(200);
+      response.end(templete);
+      });
+    } else {
+      response.writeHead(404);
+      response.end("Not found");
+    }
 });
 
 app.listen(3000);
