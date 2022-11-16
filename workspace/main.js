7e15a4ec9a3aa2a4340413a287807d7a4943e974
마현우 <mhw35689402@gmail.com>
2022-11-16 10:55:19 +0900

    ADD :: home 구현

--- workspace/main.js
+++ workspace/main.js
@@ -6,32 +6,58 @@ var app = http.createServer((request,response) => {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-    var title = queryData.id;
     
     if(pathname === '/') {
-      fs.readFile(`./data/${queryData.id}`, 'utf-8', (err, description) => {
-      var templete = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ol>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ol>
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>    
-      `;
-      response.writeHead(200);
-      response.end(templete);
-      });
+      if(queryData.id === undefined) {
+        var title = 'Welcome';
+        var description = 'Hello, Node.js';
+        var templete = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>    
+        `;
+        response.writeHead(200);
+        response.end(templete);
+      } else {
+        fs.readFile(`./data/${queryData.id}`, 'utf-8', (err, description) => {
+          var title = queryData.id;
+          var templete = `
+            <!doctype html>
+            <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              <ol>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+              </ol>
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </body>
+            </html>    
+          `;
+          response.writeHead(200);
+          response.end(templete);
+        });
+      }
     } else {
       response.writeHead(404);
       response.end("Not found");
