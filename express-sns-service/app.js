29ce4ce112653dd9cc7488aca48d6b799b3786bc
마현우 <mhw35689402@gmail.com>
2023-01-29 23:12:57 +0900

    UPDATE :: add passport configuration

--- express-sns-service/app.js
+++ express-sns-service/app.js
@@ -5,10 +5,12 @@ const path = require('path');
 const session = require('express-session');
 const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
+const passport = require('passport');
 
 dotenv.config();
 const pageRouter = require('./routes/page');
 const { sequelize } = require('./models');
+const passportConfig = require('./passport');
 
 const app = express();
 app.set('port', process.env.PORT || 8001);
@@ -39,6 +41,8 @@ app.use(session({
         secure: false,
     },
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', pageRouter);
 
